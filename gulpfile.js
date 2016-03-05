5a29f84ebbc420eecfcfe7b6bed303a92bf9df49
Mathieu Maes <mathieu@webberig.be>
2016-03-05 16:19:04 +0100

    MDL + timeline + header

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,14 @@ gulp.task('sass', function () {
         .pipe(sass({
             includePaths: [
                 path.resolve(config.src, "sass"),
-                path.resolve("bower_components")
+                path.resolve("bower_components"),
+                path.resolve("bower_components/material-design-lite/src"),
+                path.resolve("bower_components/compass-mixins/lib"),
             ]
         }).on('error', sass.logError))
         .pipe(gulp.dest(path.resolve(config.dist, 'css')));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch(path.resolve(config.src,  'sass/**/*.scss'), ['sass']);
 });
